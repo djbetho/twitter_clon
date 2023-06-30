27d34d4c120e84d492b5e4428d9fb33bffdd8cf0
Ruperto <ru.bugueno@gmail.com>
2023-06-30 01:45:24 -0400

    adding pagy pgsearch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    Pagy::DEFAULT[:items] = 10
+    Pagy::DEFAULT[:overflow] = :last_page
+
+    include Pagy::Backend
+
+
 end
--- app/models/twitter.rb
+++ app/models/twitter.rb
@@ -1,2 +1,8 @@
 class Twitter < ApplicationRecord
+    include PgSearch::Model
+ pg_search_scope :search_full_text,
+against: {
+    desciption: 'A',
+    username: 'B',
+ }
 end
